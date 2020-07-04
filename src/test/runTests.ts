0c4165841b5c61ee6bfb812b6500f0d4318ceb70
Ron Netzer <ron@e-square.io>
2020-07-04 22:24:31 +0300

    fix pipeline tests not found

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -1,19 +1,11 @@
-import * as cp from 'child_process';
 import * as path from 'path';
-import { runTests, downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
+import { runTests, downloadAndUnzipVSCode } from 'vscode-test';
 
 async function go() {
 	try {
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 		const extensionTestsPath = path.resolve(__dirname, './');
 		const vscodeExecutablePath = await downloadAndUnzipVSCode('insiders');
-		const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-
-		// Use cp.spawn / cp.exec for custom setup
-		cp.spawnSync(cliPath, ['--install-extension', 'github.vscode-pull-request-github'], {
-			encoding: 'utf-8',
-			stdio: 'inherit'
-		});
 
 		/**
 		 * Basic usage
